06b647e1de4bac11279cac5e151a0428428a67ec
Dylan Elliott <dylan.elliott@gmail.com>
2022-02-19 10:11:43 +1100

    Initial Commit

---
+++ Holes/HolesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolesApp.swift
+//  Holes
+//
+//  Created by Dylan Elliott on 19/2/2022.
+//
+
+import SwiftUI
+
+@main
+struct HolesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
